5a41808dec2481c4365ffcaf2b923bf6a780e2b1
lerman251 <lthompson2023@gmail.com>
2023-03-07 09:00:06 -0600

    done

--- jpf_hitori.js
+++ jpf_hitori.js
@@ -46,100 +46,187 @@
       the numbers, blocks, and rating parameters.
 	
 */
-window.onload = init
+var allCells;
 
-var puzzleCells;
+window.onload = startUp;
 
-function init() {
-   document.getElementById("puzzleTitle").innerHTML
-   document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
-   var puzzleButtons = document.getElementsByClassName("puzzles");
-   for (var i = 0; i <puzzleButtons.length; i++) {
-      puzzleButtons[i].onclick = swapPuzzle;
-   }
-   setupPuzzle();
-   
+function startUp() {
+  document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
+  document.getElementById("puzzle").innerHTML = drawHitori(
+    hitori1Numbers,
+    hitori1Blocks,
+    hitori1Rating
+  );
+
+  var puzzleButtons = document.getElementsByClassName("puzzles");
+  for (var i = 0; i < puzzleButtons.length; i++) {
+    puzzleButtons[i].onclick = switchPuzzle;
+  }
+
+  setupPuzzle();
+
+  document.getElementById("check").onclick = findErrors;
+  document.getElementById("solve").onclick = showSolution;
 }
 
+function switchPuzzle(e) {
+  if (confirm("Switching puzzles will erase your work. Are you sure?")) {
+    var puzzleID = e.target.id;
+    document.getElementById("puzzleTitle").innerHTML = e.target.value;
+    switch (puzzleID) {
+      case "puzzle1":
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori1Numbers,
+          hitori1Blocks,
+          hitori1Rating
+        );
+        break;
+      case "puzzle2":
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori2Numbers,
+          hitori2Blocks,
+          hitori2Rating
+        );
+        break;
+      case "puzzle3":
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori3Numbers,
+          hitori3Blocks,
+          hitori3Rating
+        );
+        break;
+    }
+    setupPuzzle();
+  }
+}
 
+function setupPuzzle() {
+  allCells = document.querySelectorAll("#hitoriGrid td");
+  for (var i = 0; i < allCells.length; i++) {
+    allCells[i].style.backgroundColor = "white";
+    allCells[i].style.color = "black";
+    allCells[i].style.borderRadius = "0";
+    allCells[i].addEventListener("mousedown", function (e) {
+      if (e.shiftKey) {
+        this.style.backgroundColor = "white";
+        this.style.color = "black";
+        this.style.borderRadius = "0";
+      } else if (e.altKey) {
+        this.style.backgroundColor = "black";
+        this.style.color = "white";
+        this.style.borderRadius = "0";
+      } else {
+        this.style.backgroundColor = "rgb(101, 101, 101)";
+        this.style.color = "white";
+        this.style.borderRadius = "50%";
+      }
+      e.preventDefault();
+    });
+    allCells[i].addEventListener("mouseover", function (e) {
+      if (e.shiftKey) {
+        this.style.cursor = "url(jpf_eraser.png), alias";
+      } else if (e.altKey) {
+        this.style.cursor = "url(jpf_block.png), cell";
+      } else {
+        this.style.cursor = "url(jpf_circle.png), pointer";
+      }
+    });
+    allCells[i].addEventListener("mouseup", checkSolution);
+  }
+}
 
+function findErrors() {
+  for (var i = 0; i < allCells.length; i++) {
+    if (
+      (allCells[i].className == "blocks" &&
+        allCells[i].style.backgroundColor == "rgb(101, 101, 101)") ||
+      (allCells[i].className == "circles" &&
+        allCells[i].style.backgroundColor == "black")
+    ) {
+      allCells[i].style.color = "red";
+    }
+  }
+  setTimeout(function () {
+    for (var i = 0; i < allCells.length; i++) {
+      if (allCells[i].style.color == "red") {
+        allCells[i].style.color = "white";
+      }
+    }
+  }, 1000);
+}
 
-
-
-
-         
 /* ================================================================= */
 
 function checkSolution() {
-   /* Set the initial solved state of the puzzle to true */
-   var solved = true;
+  /* Set the initial solved state of the puzzle to true */
+  var solved = true;
 
-   /* Loop through the puzzle cells, exiting when an incorrect
+  /* Loop through the puzzle cells, exiting when an incorrect
       cell is found, setting the solved variable to false */
 
-   for (var i = 0; i < allCells.length; i++) {
-      var cellColor = allCells[i].style.backgroundColor;
-      var cellClass = allCells[i].className;
+  for (var i = 0; i < allCells.length; i++) {
+    var cellColor = allCells[i].style.backgroundColor;
+    var cellClass = allCells[i].className;
 
-      /* A cell is incorrect if it is in the block class and is not black
+    /* A cell is incorrect if it is in the block class and is not black
          or in the circle class and is not white */
-      if ( (cellClass == "blocks" && cellColor !== "black") || 
-           (cellClass == "circles" && cellColor !== "rgb(101, 101, 101)")) {
-         solved = false;
-         break;
-      }
-   }
+    if (
+      (cellClass == "blocks" && cellColor !== "black") ||
+      (cellClass == "circles" && cellColor !== "rgb(101, 101, 101)")
+    ) {
+      solved = false;
+      break;
+    }
+  }
 
-   /* If solved is still true after the loop, display an alert box */
-   if (solved) alert("Congratulations! You solved the puzzle!");
+  /* If solved is still true after the loop, display an alert box */
+  if (solved) alert("Congratulations! You solved the puzzle!");
 }
 
-function showSolution () {
-   for (var i = 0; i < allCells.length; i++) {
-      allCells[i].style.color = "";
-      allCells[i].style.backgroundColor = "";
-      allCells[i].style.borderRadius = "";
-   };   
+function showSolution() {
+  for (var i = 0; i < allCells.length; i++) {
+    allCells[i].style.color = "";
+    allCells[i].style.backgroundColor = "";
+    allCells[i].style.borderRadius = "";
+  }
 }
 
 function drawHitori(numbers, blocks, rating) {
+  /* Initial HTML String for the Hitori Puzzle */
+  var htmlString = "";
 
-   /* Initial HTML String for the Hitori Puzzle */
-   var htmlString = "";
-
-   /* numbers is a multidimensional array containing the
+  /* numbers is a multidimensional array containing the
       Hitori numbers; blocks is a corresponding 
       multidimensional array containing the location of the
       blocks which are indicated by the # character.
       Non-blocking cells are indicated by a blank character.
   */
 
-   /* Create a Web table with the id, hitoriGrid, containing
+  /* Create a Web table with the id, hitoriGrid, containing
       the numeric values. Blocks cells have the class name,
       blocks. Non-blocking cells have the class name, circles
   */
 
-   var totalRows = numbers.length;
-   var totalCols = numbers[0].length;
-   htmlString = "<table id='hitoriGrid'>";
-   htmlString += "<caption>" + rating + "</caption>";
-   
+  var totalRows = numbers.length;
+  var totalCols = numbers[0].length;
+  htmlString = "<table id='hitoriGrid'>";
+  htmlString += "<caption>" + rating + "</caption>";
 
-   for (var i = 0; i < totalRows; i++) {
-      htmlString += "<tr>";
+  for (var i = 0; i < totalRows; i++) {
+    htmlString += "<tr>";
 
-      for (var j = 0; j < totalCols; j++) {
-         if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>"
-         else htmlString += "<td class='circles'>";
+    for (var j = 0; j < totalCols; j++) {
+      if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>";
+      else htmlString += "<td class='circles'>";
 
-         htmlString += numbers[i][j];
-         htmlString +="</td>";
-      }
+      htmlString += numbers[i][j];
+      htmlString += "</td>";
+    }
 
-      htmlString += "</tr>";
-   }
+    htmlString += "</tr>";
+  }
 
-   htmlString += "</table>";
+  htmlString += "</table>";
 
-   return htmlString;
+  return htmlString;
 }
